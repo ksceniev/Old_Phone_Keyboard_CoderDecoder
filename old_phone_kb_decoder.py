7f889dfd9b1c22955ffb22a842c9d3463aba6b5f
kanjiceniev <55764096+kanjiceniev@users.noreply.github.com>
2021-04-01 18:24:52 -0300

    Update old_phone_kb_decoder.py

--- old_phone_kb_decoder.py
+++ old_phone_kb_decoder.py
@@ -1,11 +1,29 @@
-import os
+import sys, getopt
 import itertools
 
 decoder_dict = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-os.chdir('/home/annie/Documents/htb/boxes')
+
+# gets the inpute file in command line
+def main(argv):
+   inputfile = str()
+   outputfile = str()
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print('test.py -i <inputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   return inputfile
 
 lines = []
-fobj = open('bank_heist_message.txt', 'r+')
+fobj = open(main(sys.argv[1:]), 'r+')
 for line in fobj:
     lines.append(line)
 fobj.close()
@@ -28,10 +46,9 @@ for a in listed_nums:
         except ValueError:
             a.remove(i)
 
-for a in listed_nums:
+for a in listed_nums:    
     letters = []
     l = [(k, sum(1 for _ in v)) for k, v in itertools.groupby(a)] # list of tuples containing the key pressed and how many times it was pressed.
-    
     for t in l: 
         k, v = t[0], t[1]
         letter = decoder_dict[k][v-1]
